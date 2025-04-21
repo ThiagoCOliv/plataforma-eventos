881f4b5fb3118996d0baa63401456412b2d1421c
Thiago Correa <thiagocdo445@gmail.com>
2025-04-21 13:55:21 -0300

    Adição do componente de login ao Navbar e implementação da lógica para abrir o modal de login.

--- plataforma-eventos-front/src/app/components/modal/login/login.component.ts
+++ plataforma-eventos-front/src/app/components/modal/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  @Input() open: boolean = false;
 }
--- plataforma-eventos-front/src/app/components/navbar/navbar.component.ts
+++ plataforma-eventos-front/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "../modal/login/login.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
 
+export class NavbarComponent 
+{
+  dialogsOpen = {
+    login: false,
+    cadastro: false,
+    cadastroMessage: false
+  }
+
+  abrirModal(modal: string) 
+  {
+    Object.keys(this.dialogsOpen).forEach((key) => this.dialogsOpen[key as keyof typeof this.dialogsOpen] = key == modal ? true : false);
+  }
 }
